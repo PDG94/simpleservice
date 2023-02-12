90caf5afa1f1e4327fc3c304dbbdb7546a49e2a9
Edwards Ardila <115187420+Edwards0307@users.noreply.github.com>
2023-02-12 12:49:15 -0500

    Components

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import Card from "./Card/Card";
+import CardsContainer from "./CardsContainer/CardsContainer";
+import Filter from "./Filter/Filter";
+import NavBar from "./NavBar/NavBar";
+import Paged from "./Paged/Paged";
+import SearchBar from "./SearchBar/SearchBar";
+import Order from "./Order/Order";
+
+export { Card, CardsContainer, Filter, NavBar, Order, Paged, SearchBar };
