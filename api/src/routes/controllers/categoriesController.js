8d14c74d0dd695f6380ac8f4cbfbc5e8a4fa8d2b
PDG94 <ranekpdg@gmail.com>
2023-02-13 20:21:31 -0300

    post category 13/02

--- api/src/routes/controllers/categoriesController.js
+++ api/src/routes/controllers/categoriesController.js
@@ -5,5 +5,15 @@ const getAllCategories = async() =>{
     return await Category.findAll();
 }
 
+const createCategory = async({name, description=null}) =>{
 
-module.exports = {getAllCategories}
+    const newCategory = await Category.create({
+        name, description
+    })
+
+    return newCategory;
+
+}
+
+
+module.exports = {getAllCategories, createCategory}
