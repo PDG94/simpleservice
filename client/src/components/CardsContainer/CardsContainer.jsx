8df8e9541f08f52768e6b77483554c163830d73b
maggieruizdiaz20 <105393918+maggieruizdiaz20@users.noreply.github.com>
2023-02-13 18:28:17 -0500

    cambios

--- client/src/components/CardsContainer/CardsContainer.jsx
+++ client/src/components/CardsContainer/CardsContainer.jsx
@@ -1,3 +1,17 @@
-import '../CardsContainer/cardsContainer.css'
+import '../CardsContainer/cardsContainer.css';
+import { useSelector } from 'react-redux';
 
-export default function CardsContainer() {}
+export default function CardsContainer() {
+
+       const allServices = useSelector((state)=>state.services)
+       const [ currentPage, setCurrentPage ] = useState(1) // seteo la pagina para que empiece en 1
+       const [ servicesPerPage, setServicesPerPage ] = useState(6) // servicios por pagina
+       const indexOfLastService = currentPage*servicesPerPage // 6
+       const indexOfFirstService = indexOfLastService - servicesPerPage // 6-6 = 0
+       const currentServices = allServices.slice(indexOfFirstService,indexOfLastService)
+
+
+       const paged=(pageNumber)=>{
+        return setCurrentPage(pageNumber);
+       };
+}
