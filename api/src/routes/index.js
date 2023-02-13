4d0f969ac415908470e43fcad0a4c796d8ae9a0b
David <cdvdrosero@gmail.com>
2023-02-13 15:21:15 -0500

    (WIP)added routes skeleton 13/02/2023

---
+++ api/src/routes/index.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+//require routes here
+
+const router = Router();
+
+// router.use('/services', put imported route function here );
+
+// router.use('/users', put imported route function here);
+
+// router.use('/categories', put imported route function here )
+
+module.exports = router;
