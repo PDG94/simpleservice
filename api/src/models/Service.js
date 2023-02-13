4ac6d6f76c74c6de62d195552796012c2f68c5c9
David <cdvdrosero@gmail.com>
2023-02-13 14:15:19 -0500

    First iteration of Service model

--- api/src/models/Service.js
+++ api/src/models/Service.js
@@ -1,1 +1,26 @@
-//comment
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define('Service', {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        name:{
+            type: DataTypes.STRING,
+        },
+        image:{
+            type: DataTypes.STRING,
+        },
+        description:{
+            type: DataTypes.STRING,
+        },
+        price:{
+            type: DataTypes.FLOAT,
+        },
+        active:{
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        }
+    })
+}
