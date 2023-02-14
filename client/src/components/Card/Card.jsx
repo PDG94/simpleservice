54a0a0077e8df05aa09f6430e6bd413d537b41c1
LuHeredia <102454015+LuHeredia@users.noreply.github.com>
2023-02-14 16:26:24 -0300

    chau botstrap

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,16 +5,12 @@ export default function Card({ id, name, image, price, active }) {
   return (
       <Link to={`/Detail/${id}`}>
         <div class="row">
-        <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4" >
-        <div class="card p-0 overflow-hidden h-100 shadow" >
-        <img class="card-img-top img-fluid" src={image} alt="Card image cap"/>
-        <div class="card-body">
-        <h2 class="card-title">{name}</h2>
-        <p class="card-text">Price: {price}</p>
-        <p class="card-text">Active: {active}</p>
-        <a href="#" class="btn btn-primary">Get it</a>
-        </div>
-        </div>
+        <img class="image "src={image} alt=""/>
+        <div class="cardBody">
+        <h2 class="cardTitle">{name}</h2>
+        <p class="cardPrice">Price: {price}</p>
+        <p class="cardActive">Active: {active}</p>
+        <button class="button">Get it</button>
         </div>
         </div>
         </Link>
