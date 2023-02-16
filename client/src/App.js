1e1c202b0f77c2d6d17de095859fd91da661004a
maggieruizdiaz20 <105393918+maggieruizdiaz20@users.noreply.github.com>
2023-02-15 22:36:02 -0500

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Login from "./components/Pages/Login"
+import Detail from "./components/Detail/Detail";
+import Register from "./components/Pages/Register"
+import { Reset } from "./components/Pages/Reset";
+import { NavBar } from "./components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
     <BrowserRouter>
+    <ToastContainer/>
+    <NavBar/>
       {" "}
       {/* inyecta propiedades a nuesto componente para acceder al historial de navegacion */}
       <div className="App">
@@ -13,8 +24,13 @@ function App() {
           {/* permite navegar de ruta a ruta es buena practica aplicarlo*/}
           <Route exact path="/" element={<Home />} />{" "}
           {/* componente que utilizamos para crear nuestras rutas a otras paginas*/}
-        </Routes>
+          <Route path = "/login" element={<Login/>} />
+          <Route path = "/detail:id" element={<Detail/>} />
+          <Route path= "/register" element={<Register/>} />
+          <Route path= "/reset" element={<Reset/>} />
+          </Routes>
       </div>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,76 +1,69 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { SearchBar } from "../index";
 import "../NavBar/navBar.css";
+import { auth } from "../../components/Firebase/config"
+import { toast } from "react-toastify";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import {BsFillCartCheckFill} from 'react-icons/bs'
+import { useEffect, useState } from "react";
+import {FaUserCircle} from "react-icons/fa"
 
 const NavBar = () => {
-  const handleChange = () => {};
+  const navigate = useNavigate()
+  const[displayName,setDisplayName] = useState("")
+ // monitores si estas logueado y muestra el nombre del usuario en la barra de nav 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+      
+        const uid = user.uid;
+        console.log(user.displayName);
+        setDisplayName(user.displayName)  
+      } else {
+        setDisplayName("")
+      }
+    });
+  },[])
+      
+  function logoutUser(){
+    signOut(auth).then(() => {
+      toast.success("Logout successfully.")
+      navigate("/")
+    }).catch((error) => {
+      toast.error(error.message)
+    });
+  }
+
+
+
   return (
 <ul>
   <Link to="/">
   <li><a href="#home">Home</a></li>
   </Link>
-  <Link to="/">
+  <Link to="/catalog">
   <li><a href="#catalog">Catalog</a></li>
   </Link>
-  <span> 
-  <Link to="/">
+  <Link to="/carrito">
   <span>
   <li><a href="#icon"><BsFillCartCheckFill/></a></li>
   </span>
-  <span> 
-  <Link to="/">
-  <li><a href="#signUp">Login</a></li>
   </Link>
+  <Link to="/login">
+  <span> 
+  <li><a href="#signUp">Login</a></li>
    </span> 
+   </Link>
+   <a href="#"><FaUserCircle size={16}/>
+   Hi, {displayName}
+   </a>
+   <Link to="register">
    <li><a href="#signIn">Register</a></li>
   </Link>
-  </span>
-  
-  <Link to="/">
-
- 
-  
+  <Link to="/" onClick={logoutUser}>
+   <li><a href="#logout">Logout</a></li>
   </Link>
 </ul>
   )
 }
-
-
-
-
-
-
-
-
-
-    
-//     <ul>
-//     <div className="mainContainer">
-//       <Link to="/">
-//         <button onClick={handleChange}>Home</button>
-//       </Link>
-      
-//       <Link to="/">
-//         <button onClick={handleChange}>Catalog</button>
-//       </Link>
-
-//       <Link to="/">
-//         <button onClick={handleChange}>Sign In</button>
-//         </Link>
-
-//         <Link to="/">
-//         <button onClick={handleChange}>Sing Up</button>
-//       </Link>
-
-//       <Link to="/">
-//           <BsFillCartCheckFill size={20} />
-//       </Link>
-
-//       <SearchBar />
-//     </div>
-//     </ul>
-//   );
-// };
-
 export default NavBar;
