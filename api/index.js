dbf2c7ee2c7c442e273b1dc8fd30af17ca690f3e
PDG94 <ranekpdg@gmail.com>
2023-02-15 21:56:12 -0300

    proceso de deployado 15/02

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const server = require("./src/app");
 const {conn} = require("./src/db");
-
+require ('dotenv').config()
+const {PORT} = process.env
 conn.sync({alter:true}).then(()=>{
-    server.listen(3001, () =>{
+    server.listen(PORT, () =>{
         console.log("server listening at 3001")
     })
 })
