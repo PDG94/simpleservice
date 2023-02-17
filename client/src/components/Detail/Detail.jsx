cdff4524d9fb1840b6ca71949b7d254c6bd86a54
Edwards Ardila <115187420+Edwards0307@users.noreply.github.com>
2023-02-17 01:36:55 -0500

    Detail Listo

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,61 +1,54 @@
-import React, {useEffect } from "react";
-import { useSelector /*, useDispatch*/ } from "react-redux";
-import { Link, useParams } from "react-router-dom";
-// import { cleanDetail, getDetail } from "../../redux/actions";
-import "../Detail/detail.css";
-import Loading from "../Loading/Loading";
-import {NavBar, Footer} from "../index";
+import { NavBar, Footer } from "../index";
+import { getServicesDetail, cleanState } from "../../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Detail (){
+export default function Detail() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const serviceDetail = useSelector((state) => state.serviceDetail);
 
-    const { id } = useParams();
-    // const dispatch = useDispatch();
-    
-    // useEffect(() => {
-    //     dispatch(getDetail(id));
-    //     return () => {
-    //         dispatch(cleanDetail())
-    //     }
-    // }, [dispatch, id]);
+  useEffect(() => {
+    dispatch(cleanState());
+    dispatch(getServicesDetail(id));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    //PARA QUE HAGA SCROLL HACIA ARRIBA
-    useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-      });
+  if (serviceDetail.length === 0) return <h1>Cargando</h1>;
 
-    const serviceDetail = useSelector((state) => state.services);
+  return (
+    <div>
+      <NavBar />
+      <Link to={"/Services"}>
+        <button>Back</button>
+      </Link>
 
-    return (
-        <div className="detailContainer">
-            <NavBar/>
-            <div>
-                <Link to="/" className="btnHome">
-                    <button>Go Back</button>
-                </Link>
-            </div>
-            <p>Estoy en Detail</p>
-            {/* { serviceDetail.length > 0 ? (
-                <div className="main">
-                    <div className="left">
-                        <div className="details">
-                            <h1 className="name">{serviceDetail.name || "name not found"}</h1>
-                            <div className="info">
-                            <p className="activity">{serviceDetail.acivity || "activity not found"}</p>
-                            <p className="price">{serviceDetail.price}</p>
-                            <div className="description">{serviceDetail.description || "description not found"}</div>
-                            </div>
-                        </div>
-                        <div className="right">
-                            {serviceDetail.image ? (
-                                <img src={serviceDetail.image || "image not found"} alt={`${serviceDetail.name}`} width="275vh" className="image"/>
-                            ): <img alt="default" src="una imagen de default"/>}
-                        </div>
-                    </div>
-                </div>
-            ):<Loading/>} */}
-            <Footer/>
-        </div>
-    )
+      {serviceDetail.map((service) => {
+        return (
+          <div key={service.id}>
+            <h1>{service?.username}</h1>
+
+            <img src={service?.userimage} alt="" />
+
+            <br></br>
+
+            <label>Service Name: </label>
+            <p>{service?.servicename}</p>
+
+            <label>Rating: </label>
+            <p>{service?.rating}</p>
+
+            <label>Price: </label>
+            <p>{service?.price}</p>
+
+            <label>Description: </label>
+            <p>{service?.description}</p>
+          </div>
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
-
-
--- client/src/redux/actionTypes.js
+++ client/src/redux/actionTypes.js
@@ -1,5 +1,5 @@
 export const GET_SERVICES = "GET_SERVICES";
+export const GET_SERVICES_DETAIL = "GET_SERVICES_DETAIL";
+export const CLEAN_STATE = "CLEAN_STATE";
 export const CLEANER_NAME = "CLEANER_NAME";
 export const GET_BY_NAME = "GET_BY_NAME";
-export const GET_DETAIL = "GET_DETAIL";
-export const CLEAN_DETAIL = "CLEAN_DETAIL";
