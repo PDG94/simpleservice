303170a37c91636d1897e0c1060c444f650948b0
maggieruizdiaz20 <105393918+maggieruizdiaz20@users.noreply.github.com>
2023-02-21 21:39:25 -0500

    prohibited al create

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,11 @@ import {
   Create,
   NotFound,
   Landing,
-  Admin,
-  AdminOnlyRoute,
+  Prohibido
+  
 
 } from "./components/index";
+
 
 function App() {
   return (
@@ -44,12 +45,14 @@ function App() {
           <Route path="/register" element={<Register />} />
 
           <Route path="/reset" element={<Reset />} />
-
+          
+          <Route path="/prohibited" element={<Prohibido/>} />
+{/* 
           <Route path= "/admin/*" element={
           <AdminOnlyRoute> 
           <Admin/>
           </AdminOnlyRoute>
-        } />
+        } /> */}
 
         </Routes>
       </div>
