797afd254b2542c9d22af4afe40acb4c725faf23
David <cdvdrosero@gmail.com>
2023-02-23 12:23:19 -0500

    cambio inhecho

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app");
 const {conn} = require("./src/db");
 require ('dotenv').config()
 const {PORT} = process.env
-conn.sync({force:true}).then(()=>{
+conn.sync({alter:true}).then(()=>{
     server.listen(PORT, () =>{
         console.log("server listening at 3001")
     })
