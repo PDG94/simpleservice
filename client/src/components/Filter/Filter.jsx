28ab3417f7cc5813c607ba67cbbbdc8c72fdec23
Ash19-88 <ayelenllampa88@gmail.com>
2023-02-25 17:42:39 -0300

    alineando cosas

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -46,13 +46,13 @@ export default function Filter() {
 
   return (
     <div className="filter">
-      <li>
-        <span className="titleFilter">Filters & Sorts</span>
+      <div>
+        <h1 className="titleFil">Filters & Sorts</h1>
 
         {/*CATEGORIAS*/}
         <br />
-        <span className="spantitle">Select a category</span>
-        <select onChange={(e) => handleCategory(e)}>
+        <span className="spanFilter">Select a category</span>
+        <select className="selectFilter" onChange={(e) => handleCategory(e)}>
           <option value="all">Categories</option>
           {categories?.map((elem) => (
             <option key={elem.id} value={elem.id}>
@@ -62,24 +62,24 @@ export default function Filter() {
         </select>
 
         {/*PRECIO*/}
-        <span className="spantitle">Order services by </span>
-        <select onChange={(e) => handlePrice(e)}>
+        <span className="spanFilter">Order services by </span>
+        <select className="selectFilter" onChange={(e) => handlePrice(e)}>
           <option value="all">Price</option>
           <option value="DESC">More Price</option>
           <option value="ASC">Low Price</option>
         </select>
 
         {/*ABC*/}
-        <span className="spantitle">Order services by </span>
-        <select onChange={(e) => handleName(e)}>
+        <span className="spanFilter">Order services by </span>
+        <select className="selectFilter" onChange={(e) => handleName(e)}>
           <option value="all">Alphabet</option>
           <option value="ASC">A-Z</option>
           <option value="DESC">Z-A</option>
         </select>
 
         {/*RATING*/}
-        <span className="spantitle">Order services by </span>
-        <select onChange={(e) => handleRating(e)}>
+        <span className="spanFilter">Order services by </span>
+        <select className="selectFilter" onChange={(e) => handleRating(e)}>
           <option value="all">Rating</option>
           <option value="DESC">More Rating</option>
           <option value="ASC">Low Rating</option>
@@ -88,7 +88,7 @@ export default function Filter() {
         <button className="btnFilter" onClick={() => handleSubmit()}>
           Press to Filter
         </button>
-      </li>
+      </div>
     </div>
   );
 }
