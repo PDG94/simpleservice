2396ca454e1f071224cfc75178a116a474e5ec09
PDG94 <ranekpdg@gmail.com>
2023-02-27 13:10:21 -0300

    stripe funcionando v2

--- client/src/components/Pages/Checkout/CheckoutDetails.jsx
+++ client/src/components/Pages/Checkout/CheckoutDetails.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { toast } from "react-toastify";
 import { loadStripe } from "@stripe/stripe-js";
 import s from "../Checkout/checkoutDetails.css";
+import CheckoutSummary from "../../CheckoutSummary/CheckoutSummary"
 import {
   Elements,
   CardElement,
@@ -8,13 +10,34 @@ import {
   useElements,
 } from "@stripe/react-stripe-js";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import {clearCart} from "../../../redux/actions";
+import {useNavigate} from "react-router-dom"
 const stripePromise = loadStripe(
   `pk_test_51MeScXEohVMDTuBfkv6jlBnpXq6EN6W0vJs3bFlepyOusbfYEuIAhoOXcsYFGgcDcOqwJLAqYL4qqNegKOdGJOvE00lBepiZlb`
 );
 
 const CheckOutForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const stripe = useStripe();
   const elements = useElements();
+  const cartTotalAmount = useSelector((state) => state.cartTotalAmount);
+  const totalPayment= (parseFloat(cartTotalAmount.toFixed(2), 0))*100
+  const paraInvocar=()=>{
+    window.location.reload(true)
+  }
+  const clearCart1 = () => {    
+    dispatch(clearCart());     
+    navigate("/home");
+    paraInvocar();  
+  };
+
+  const clearAndBack=()=>{
+    setTimeout(clearCart1, 3000)    
+  }
+  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -33,12 +56,13 @@ const CheckOutForm = () => {
           "https://simpleservice-production.up.railway.app/checkout",
           {
             id,
-            amount: 10000, //cents
+            amount: totalPayment, 
           }
         );
         console.log(data);
 
         elements.getElement(CardElement).clear();
+        toast.success("Payment Succesfully!")
       } catch (error) {
         console.log(error);
       }
@@ -47,9 +71,15 @@ const CheckOutForm = () => {
   return (
     <div className={s.containerCheckDetail}>
       <form onSubmit={handleSubmit}>
+      <div className="">
+                  <h4>Subtotal:</h4>
+                  <h3>{`$${cartTotalAmount.toFixed(2)}`}</h3>
+                </div>
         <h2 className={s.labelCheck}>Introduce tu metodo de pago</h2>
         <CardElement className={s.inputCheck} />
-        <button className={s.btnCheckDet}>Buy</button>
+        
+        <button className={s.btnCheckDet} onClick={clearAndBack}>Buy</button>
+        <CheckoutSummary/>
       </form>
     </div>
   );
