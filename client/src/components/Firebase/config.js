c28ff1c9d453753fb62ec02951c978b571c8fc61
David <cdvdrosero@gmail.com>
2023-03-03 15:01:43 -0500

    upload service image function

--- client/src/components/Firebase/config.js
+++ client/src/components/Firebase/config.js
@@ -21,10 +21,19 @@ export const storage = getStorage(app);
 export default app;
 
 export async function uploadFile(file, uid) {
-  if(!file){
-    return "https://firebasestorage.googleapis.com/v0/b/henry-proyecto-final.appspot.com/o/users%2Fimagen_2023-02-25_173305172.png?alt=media&token=9ec2b9fe-7c1c-40ad-b466-6761c9bf3c7a"
+  if (!file) {
+    return "https://firebasestorage.googleapis.com/v0/b/henry-proyecto-final.appspot.com/o/users%2Fimagen_2023-02-25_173305172.png?alt=media&token=9ec2b9fe-7c1c-40ad-b466-6761c9bf3c7a";
   }
   const storageRef = ref(storage, `users/${uid}`);
+  await uploadBytes(storageRef, file);
+  return await getDownloadURL(storageRef);
+}
+
+export async function uploadCardFile(file, uuid) {
+  if (!file) {
+    return null;
+  }
+  const storageRef = ref(storage, `services/${uuid}`);
   await uploadBytes(storageRef, file);
   return await getDownloadURL(storageRef);
 }
