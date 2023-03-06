d4e5216d5b4b051a4ba8440757b04487062fc20e
David <cdvdrosero@gmail.com>
2023-03-06 16:51:11 -0500

    hidden links

--- client/src/components/HiddenLinks/ShowOnLogout.jsx
+++ client/src/components/HiddenLinks/ShowOnLogout.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 // hidde logout botton or show it
 
 function ShowOnLogout({ children }) {
-  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.users.isLoggedIn);
   if (!isLoggedIn) {
     return children;
   }
