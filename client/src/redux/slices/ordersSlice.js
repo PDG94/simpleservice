536898e67a4755a78ff6c6029ad032ddd5607c6a
David <cdvdrosero@gmail.com>
2023-03-06 18:48:41 -0500

    minor changes and minor fixes

--- client/src/redux/slices/ordersSlice.js
+++ client/src/redux/slices/ordersSlice.js
@@ -1,8 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const persistentHist =
+  localStorage.getItem("orderHistory") !== null
+    ? JSON.parse(localStorage.getItem("orderHistory"))
+    : [];
+
+const persistentAmount =
+  localStorage.getItem("totalOrderAmount") !== null
+    ? JSON.parse(localStorage.getItem("totalOrderAmount"))
+    : null;
+
 const initialState = {
-  orderHistory: [],
-  totalOrderAmount: null,
+  orderHistory: persistentHist,
+  totalOrderAmount: persistentAmount,
 };
 
 export const ordersSlice = createSlice({
@@ -11,9 +21,17 @@ export const ordersSlice = createSlice({
   reducers: {
     setOrders: (state, action) => {
       state.orderHistory = action.payload;
+      localStorage.setItem(
+        "orderHistory",
+        JSON.stringify(state.orderHistory.map((item) => item))
+      );
     },
     orderAmount: (state, action) => {
       state.totalOrderAmount = action.payload;
+      localStorage.setItem(
+        "totalOrderAmount",
+        JSON.stringify(state.totalOrderAmount)
+      );
     },
   },
 });
