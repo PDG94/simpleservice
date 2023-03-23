9e19dde4817a317c45ba837fd102709f33592b4e
Edwards Ardila <edwards.ardila03@gmail.com>
2023-03-22 22:27:10 -0500

    Admin

--- client/src/components/AdminOnlyRoutes/AdminOnlyRoute.jsx
+++ client/src/components/AdminOnlyRoutes/AdminOnlyRoute.jsx
@@ -3,12 +3,12 @@ import { useSelector } from "react-redux";
 const AdminOnlyRoute = ({ children }) => {
   const userEmail = useSelector((state) => state.users.email);
   const isLoggedIn = useSelector((state) => state.users.isLoggedIn);
-  /*  const { isAdmin } = useSelector((state) => state.users.serviceUser); */
+  const { isAdmin } = useSelector((state) => state.users.serviceUser);
 
   //no esta llenando el estado en esta ruta
 
   if (isLoggedIn) {
-    if (/* isAdmin || */ userEmail === "simpleservice@gmail.com") {
+    if (isAdmin || userEmail === "simpleservice@gmail.com") {
       return children;
     }
   }
